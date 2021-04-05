f1b4fbe747fc3f8a685af89cd987348091914919
Tim Paulus <paulus.t@gmx.at>
2021-04-05 14:46:00 +0200

    Initial C# commit

---
+++ CSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ParallelPasswordCracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
